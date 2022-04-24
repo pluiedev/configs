b015d7e77fbaae647894cad6810d30dcf81ed447
leocth <22406910+leocth@users.noreply.github.com>
2022-04-24 12:34:19 +0800

    deploying now preserves the old file before overwriting

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,22 @@
 # my deploy script
 # _very_ unportable, will fix later
 
+# backup_and_deploy <local_dir> <repo_dir> <file_name>
+function backup_and_deploy {
+    mv $1/$3 $1/$3.old
+    cp $2/$3 $1
+}
+
 # alacritty
-cp ./alacritty/alacritty.yml $HOME/.config/alacritty/
+backup_and_deploy $HOME/.config/alacritty alacritty alacritty.yml
 
 # nvim
-cp ./nvim/init.vim $HOME/.config/nvim/
+backup_and_deploy $HOME/.config/nvim/ nvim init.vim
 
 # tmux
-cp ./tmux/.tmux.conf $HOME/
+backup_and_deploy $HOME/ tmux .tmux.conf
 tmux source $HOME/.tmux.conf
 
 # zsh
-cp ./zsh/.zshrc $HOME/
+backup_and_deploy $HOME/ zsh .zshrc
 source $HOME/.zshrc
